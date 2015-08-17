9361a56339e322f492a7ccc225b42a2b476fab4e
Lukas Borke <lukas.borke@web.de>
2015-08-17 19:29:04 +0200

    Bugs in the abline section eliminated

--- MSMpdfasymetric.R
+++ MSMpdfasymetric.R
@@ -7,8 +7,6 @@
 #              alpha = - 5), symmetric normal and right skewed normal
 #              distribution (shape parameter alpha = 5).
 # ------------------------------------------------------------------------------
-# Usage: 
-# ------------------------------------------------------------------------------
 # Inputs: 
 # ------------------------------------------------------------------------------
 # Output: 
@@ -22,10 +20,8 @@
 # Author:      Xiu Xu 20150603
 # ------------------------------------------------------------------------------
 
-
 graphics.off()
 rm(list=ls())
-
 
 # install and load packages
 libraries = c("sn")
@@ -40,13 +36,13 @@ x2 = seq(-2, 2, 0.001)
 x3 = seq(-1, 3, 0.001)
 
 plot(x1, dsn(x1, xi = 0, omega = 1, alpha = -5, log = FALSE), type = "l", 
-     lwd = 2.5, ylab = "PDF", xlab = "X", col = "chocolate3", ylim = c(0, 
-                                                                       0.9))
-abline(v = median(rsn(x, xi = 0, omega = 1, alpha = -5)))
+     lwd = 2.5, ylab = "PDF", xlab = "X", col = "chocolate3", ylim = c(0, 0.9))
+abline(v = median(rsn(x1, xi = 0, omega = 1, alpha = -5)))
+
 plot(x2, dsn(x2, xi = 0, omega = 0.55, alpha = 0, log = FALSE), type = "l", 
-     lwd = 2.5, ylab = "", xlab = "X", col = "chartreuse4", ylim = c(0, 
-                                                                     0.9))
-abline(v = median(rsn(x, xi = 0, omega = 0.55, alpha = 0)))
+     lwd = 2.5, ylab = "", xlab = "X", col = "chartreuse4", ylim = c(0, 0.9))
+abline(v = median(rsn(x2, xi = 0, omega = 0.55, alpha = 0)))
+
 plot(x3, dsn(x3, xi = 0, omega = 1, alpha = 5, log = FALSE), type = "l", 
      lwd = 2.5, ylab = "", xlab = "X", col = "blue3", ylim = c(0, 0.9))
-abline(v = median(rsn(x, xi = 0, omega = 1, alpha = 5)))
+abline(v = median(rsn(x3, xi = 0, omega = 1, alpha = 5)))
